ce5d6a98141ec9d28dc332e899956a3cd29fc89f
ErlendA <erlend.aune@amedia.no>
2016-09-06 12:25:47 +0200

    Fixes to parsing

--- stripdoctag.py
+++ stripdoctag.py
@@ -2,9 +2,15 @@ import sys
 
 infile=sys.argv[1]
 outfile=sys.argv[2]
+
+replacesigns=[',','.','?','!',':',';','\n','(',')']
+def repsigns(txt):
+    for rs in replacesigns:
+        txt=txt.replace(rs,' '+rs+' ')
+    return ' '.join(txt.split())
 print("Infile: {0}. Outfile: {1}".format(infile,outfile))
 with open(infile,'r') as ff,open(outfile,'w') as ff2:
     for line in ff:
         if line.startswith('<doc') or line.startswith('</doc'):
             continue
-        ff2.write(line.replace('\n',' ')+' ')
+        ff2.write(repsigns(line)+' ')
